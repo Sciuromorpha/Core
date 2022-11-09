af1e317241c28221be880fb811bdb44c2ffca6db
Whale Mo <ncwhale@gmail.com>
2022-11-09 09:48:08 +0900

    Using model in src.

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -17,9 +17,8 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from sciuromorpha_core import model
+target_metadata = model.Base.metadata
 
 
 def run_migrations_offline() -> None:
