1502186ac8e4cb4ebed31b305746833fe713901f
Whale Mo <ncwhale@gmail.com>
2022-11-23 22:48:52 +0900

    Add SerializerMixin for base define.

--- src/sciuromorpha_core/db/base_class.py
+++ src/sciuromorpha_core/db/base_class.py
@@ -1,5 +1,6 @@
 from typing import Any
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy_serializer import SerializerMixin as SerializerMixinBase
 
 @as_declarative()
 class Base:
@@ -10,3 +11,6 @@ class Base:
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
+
+class SerializerMixin(SerializerMixinBase):
+    pass
