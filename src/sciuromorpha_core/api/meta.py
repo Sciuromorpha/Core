fb3a1ce38da4fe0ec9cd91a9786bec9bdbe95b93
Whale Mo <ncwhale@gmail.com>
2022-11-23 23:21:53 +0900

    Using none in merge data to delete key now.

--- src/sciuromorpha_core/api/meta.py
+++ src/sciuromorpha_core/api/meta.py
@@ -10,7 +10,7 @@ from sqlalchemy.dialects.postgresql import insert
 from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound
 
 from sciuromorpha_core.db.session import SessionFactory
-from sciuromorpha_core import model 
+from sciuromorpha_core import model
 
 
 class Meta:
@@ -21,32 +21,44 @@ class Meta:
     @rpc
     def create(self, metadata: dict):
         # Extract origin_url from meta.
-        meta = model.Meta(meta=metadata, origin_url=metadata.get('origin_url', None))
         with SessionFactory.begin() as session:
             # stmt = insert(model.Meta).values(meta=metadata, origin_url=origin_url).on_conflict_do_nothing()
             # session.execute(stmt)
-            session.add(meta)
+            meta = model.Meta(
+                meta=metadata, origin_url=metadata.get("origin_url", None)
+            )
+            with session.begin_nested():
+                session.add(meta)
+
+            result = meta.to_dict()
 
         # Publish event to other services.
-        result = meta.to_dict()
         self.dispatch("create", result)
         return result
 
     @rpc
-    def merge(self, meta_id: Union[str, UUID], metadata: dict):
+    def merge(self, meta_id: Union[str, UUID], meta_data: dict):
         # Try get meta for UPDATE
         with SessionFactory.begin() as session:
-            meta = session.get(model.Meta, id)
+            meta = session.get(model.Meta, meta_id)
             clone_meta = copy.copy(meta.meta)
 
             # Not deep clone right now.
-            for key in metadata.keys():
-                clone_meta[key] = metadata[key]
+            for key, value in meta_data.items():
+                if value is not None:
+                    clone_meta[key] = meta_data[key]
+                else:
+                    try:
+                        del clone_meta[key]
+                    except KeyError:
+                        pass
 
             meta.meta = clone_meta
-            session.add(meta)
+            with session.begin_nested():
+                session.add(meta)
 
-        result = meta.to_dict()
+            result = meta.to_dict()
+
         # Publish event to other services.
         self.dispatch("merge", result)
         return result
@@ -57,13 +69,14 @@ class Meta:
             # stmt = select(model.Meta).where(model.Meta.id == id)
             # return session.execute(stmt).first()
             meta = session.get(model.Meta, id)
-
-        return meta.to_dict()
+            return meta.to_dict()
 
     @rpc
     def get_by_origin_url(self, url: str):
         with SessionFactory.begin() as session:
-            meta = session.execute(select(model.Meta).where(model.Meta.origin_url == url)).first()
+            meta = session.execute(
+                select(model.Meta).where(model.Meta.origin_url == url)
+            ).first()
 
         return meta.to_dict()
 
