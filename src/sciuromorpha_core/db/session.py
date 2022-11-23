29ff8af0d2d314ed904dd2fac6fec17e9f6f0885
Whale Mo <ncwhale@gmail.com>
2022-11-23 15:38:22 +0900

    Rename SessionLocal to SessionFactory

--- src/sciuromorpha_core/db/session.py
+++ src/sciuromorpha_core/db/session.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import sessionmaker
 
 from sciuromorpha_core.config import config
 
-engine = create_engine(config["db"]["url"], pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(config["db"]["url"], pool_pre_ping=True, future=True)
+SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
