740d40f9806dd80c01e0eb42175e98ea56befc85
Whale Mo <ncwhale@gmail.com>
2022-11-24 18:49:38 +0900

    Move some functions to classmethod.

--- src/sciuromorpha_core/api/meta.py
+++ src/sciuromorpha_core/api/meta.py
@@ -17,6 +17,41 @@ class Meta:
 
     dispatch = EventDispatcher()
 
+    @classmethod
+    def clone_meta_data(cls, meta: model.Meta):
+        clone = copy.copy(meta.data)
+
+        if clone is not dict:
+            # Ensure this metadata is dict.
+            clone = {S.META_KEY_STUB: clone} if clone is not None else {}
+
+        return clone
+
+    @classmethod
+    def merge_meta_data(cls, origin: dict, append: dict):
+        # Merge append dict to origin.
+        # Not deep merge right now.
+
+        for key, value in append.items():
+            if value is not None:
+                origin[key] = append[key]
+            else:
+                try:
+                    del origin[key]
+                except KeyError:
+                    pass
+
+        return origin
+
+    @classmethod
+    def clone_process_tag(cls, meta: model.Meta):
+        try:
+            result = set(meta.process_tag)
+        except TypeError:
+            result = {meta.process_tag}
+
+        return result
+
     @rpc
     def create(self, meta_data: dict):
         # Extract origin_url from meta.
@@ -36,35 +71,26 @@ class Meta:
         return result
 
     @rpc
-    def merge(self, meta_id: Union[str, UUID], meta_data: dict):
-        # Try get meta for UPDATE
+    def merge(
+        self, meta_id: Union[str, UUID], meta_data: dict, process_tag: Union[None, str]
+    ):
         with SessionFactory.begin() as session:
-            meta = session.get(model.Meta, meta_id)
+            # Try get meta for UPDATE
+            meta = session.get(model.Meta, meta_id, with_for_update=True)
 
             if meta is None:
                 # Cannot merge a none
                 return None
 
-            clone_meta = copy.copy(meta.data)
-
-            if clone_meta is not dict:
-                # Ensure this metadata is dict.
-                clone_meta = (
-                    {S.META_KEY_STUB: clone_meta} if clone_meta is not None else {}
-                )
-
-            # Not deep merge right now.
-            for key, value in meta_data.items():
-                if value is not None:
-                    clone_meta[key] = meta_data[key]
-                else:
-                    try:
-                        del clone_meta[key]
-                    except KeyError:
-                        pass
-
-            meta.data = clone_meta
+            clone_meta = Meta.clone_meta_data(meta)
+            meta.data = Meta.merge_meta_data(clone_meta, meta_data)
             meta.origin_url = clone_meta.get(S.META_KEY_ORIGIN_URL, None)
+
+            if process_tag is not None:
+                clone_tags = Meta.clone_process_tag(meta)
+                clone_tags.add(process_tag)
+                meta.process_tag = list(clone_tags)
+
             with session.begin_nested():
                 session.add(meta)
 
@@ -72,6 +98,52 @@ class Meta:
 
         # Publish event to other services.
         self.dispatch("merge", result)
+        return result
+
+    @rpc
+    def add_process_tag(self, meta_id: Union[str, UUID], tag: str):
+        with SessionFactory.begin() as session:
+            # Try get meta for UPDATE
+            meta = session.get(model.Meta, meta_id, with_for_update=True)
+
+            if meta is None:
+                # Cannot merge a none, slient failed.
+                return None
+
+            clone_tags = Meta.clone_process_tag(meta)
+            clone_tags.add(tag)
+            meta.process_tag = list(clone_tags)
+
+            with session.begin_nested():
+                session.add(meta)
+
+            result = meta.to_dict()
+
+        return result
+
+    @rpc
+    def remove_process_tag(self, meta_id: Union[str, UUID], tag: str):
+        with SessionFactory.begin() as session:
+            # Try get meta for UPDATE
+            meta = session.get(model.Meta, meta_id, with_for_update=True)
+
+            if meta is None:
+                # Cannot merge a none, slient failed.
+                return None
+
+            clone_tags = Meta.clone_process_tag(meta)
+
+            try:
+                clone_tags.remove(tag)
+                meta.process_tag = list(clone_tags)
+                with session.begin_nested():
+                    session.add(meta)
+
+            except KeyError:
+                pass
+
+            result = meta.to_dict()
+
         return result
 
     @rpc
@@ -94,3 +166,47 @@ class Meta:
     @rpc
     def query(query: Any, offset: int = 0, limit: int = 100):
         pass
+
+    @rpc
+    def query_by_process_tag(self, tags: list, offset: int = 0, limit: int = 100):
+        try:
+            iter(tags)
+        except TypeError:
+            tags = [tags]
+
+        with SessionFactory.begin() as session:
+            stmt = (
+                select(model.Meta)
+                .where(model.Meta.process_tag.contains(tags))
+                .order_by(model.Meta.id)
+                .offset(offset)
+                .limit(limit)
+            )
+            rows = session.execute(stmt)
+            result = []
+            for row in rows.scalars():
+                result.append(row.to_dict())
+
+            return result
+
+    @rpc
+    def query_without_process_tag(self, tags: list, offset: int = 0, limit: int = 100):
+        try:
+            iter(tags)
+        except TypeError:
+            tags = [tags]
+
+        with SessionFactory.begin() as session:
+            stmt = (
+                select(model.Meta)
+                .where(model.Meta.process_tag.notin_(tags))
+                .order_by(model.Meta.id)
+                .offset(offset)
+                .limit(limit)
+            )
+            rows = session.execute(stmt)
+            result = []
+            for row in rows.scalars():
+                result.append(row.to_dict())
+
+            return result
