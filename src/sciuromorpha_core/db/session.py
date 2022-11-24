6357beabadd6956f1c7d2071bf17ea7473b2704e
Whale Mo <ncwhale@gmail.com>
2022-11-24 19:42:36 +0900

    Add sqlalchemy log SQL in development mode.

--- src/sciuromorpha_core/db/session.py
+++ src/sciuromorpha_core/db/session.py
@@ -1,7 +1,18 @@
+import sciuromorpha_core.static as S
+from sciuromorpha_core.config import config
+
+if config[S.CONFIG_SECTION_SCIUROMORPHA]["mode"] == S.ENV_MODE_DEVELOPMENT:
+    # We are in development mode, enable logs for SQL.
+    import logging
+
+    logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from sciuromorpha_core.config import config
-
-engine = create_engine(config["db"]["url"], pool_pre_ping=True, future=True)
-SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+engine = create_engine(
+    config[S.CONFIG_SECTION_DATABASE]["url"], pool_pre_ping=True, future=True
+)
+SessionFactory = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, future=True
+)
