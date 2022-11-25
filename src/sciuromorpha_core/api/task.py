b7be74bcfc0cb04694222e165326fa419f884788
Whale Mo <ncwhale@gmail.com>
2022-11-25 13:06:29 +0900

    Add task.create .

--- src/sciuromorpha_core/api/task.py
+++ src/sciuromorpha_core/api/task.py
@@ -1,13 +1,38 @@
+from uuid import UUID
+from typing import Any, Union
+
 from nameko.rpc import rpc
+from nameko.events import EventDispatcher
+
 from sciuromorpha_core import model, static as S
+from sciuromorpha_core.db.session import SessionFactory
 
 
 class Task:
     name = "task"
 
+    dispatch = EventDispatcher()
+
     @rpc
-    def create(self):
-        pass
+    def create(
+        self,
+        worker: str,
+        param: Any = None,
+        meta_id: Union[str, UUID, None] = None,
+        status: str = "pending",
+    ):
+        with SessionFactory.begin() as session:
+            task = model.Task(
+                worker=worker, param=param, meta_id=meta_id, status=status
+            )
+
+            with session.begin_nested():
+                session.add(task)
+
+            result = task.to_dict()
+
+        self.dispatch("create", result)
+        return result
 
     @rpc
     def update(self):
