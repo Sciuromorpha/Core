7b4775f8c275221b2bb46164872943ac59633e0c
Whale Mo <ncwhale@gmail.com>
2022-12-05 13:49:45 +0900

    Move Storage to Core service.

--- src/sciuromorpha_core/api/__init__.py
+++ src/sciuromorpha_core/api/__init__.py
@@ -1,2 +1,3 @@
 from .meta import Meta
 from .task import Task
+from .storage import Storage
---
+++ src/sciuromorpha_core/api/storage.py
@@ -0,0 +1,20 @@
+from nameko.rpc import rpc
+from nameko.events import EventHandler, EventDispatcher
+
+class Storage:
+    name="storage"
+
+    @rpc
+    def get_service_folder(self, service_meta: dict)->dict:
+        # Generate the service subfolder by the service metadata.
+        pass
+
+    def get_document(self, meta_data: dict)->dict:
+        # Calcute and return absoulate path for this metadata.
+        dir(self)
+        return {}
+
+    @rpc
+    def import_document(self, meta_data: dict)-> dict:
+        # Import document from exiting file/folder etc.
+        pass
