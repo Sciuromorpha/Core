31349504d7197588320b41b80cca1d06e24b34c0
Whale Mo <ncwhale@gmail.com>
2022-12-05 14:33:14 +0900

    Add more desc for storage path.

--- src/sciuromorpha_core/api/storage.py
+++ src/sciuromorpha_core/api/storage.py
@@ -1,3 +1,6 @@
+from uuid import UUID
+from typing import Any, Union
+
 from nameko.rpc import rpc
 from nameko.events import EventHandler, EventDispatcher
 
@@ -5,17 +8,20 @@ class Storage:
     name="storage"
 
     @rpc
-    def get_service_folder(self, service_meta: dict)->dict:
+    def get_service_path(self, service_meta: dict)->dict:
         # Generate the service subfolder by the service metadata.
+        pass
+
+    @rpc
+    def import_document(self, meta_data: dict)-> dict:
+        # Import document from exiting file/folder etc.
         pass
 
     @rpc
     def get_document(self, meta_data: dict)->dict:
         # Calcute and return absoulate path for this metadata.
-        dir(self)
         return {}
 
     @rpc
-    def import_document(self, meta_data: dict)-> dict:
-        # Import document from exiting file/folder etc.
+    def get_document_by_id(self, meta_id: Union[str, UUID])->dict:
         pass
