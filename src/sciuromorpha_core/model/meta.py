3ec5666cf0aeb61a182c344a353bad0121b892f7
Whale Mo <ncwhale@gmail.com>
2022-12-08 20:02:40 +0900

    Add unique for id column index.

--- src/sciuromorpha_core/model/meta.py
+++ src/sciuromorpha_core/model/meta.py
@@ -11,7 +11,7 @@ from sqlalchemy.dialects.postgresql import OID, UUID, BIT, ARRAY
 class Meta(Base, SerializerMixin):
     serialize_rules = ("-tasks",)
 
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, unique=True, default=uuid.uuid4)
     data = Column(JSON(none_as_null=True))
     origin_url = Column(String, index=True, unique=True)
     process_tag = Column(ARRAY(String, dimensions=1))
