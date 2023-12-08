036b7d0762be95d4da5cedced3279accbcd98b48
Whale Mo <ncwhale@gmail.com>
2023-12-08 22:34:15 +0900

    Add more settings model config.

--- src/sciuromorpha_core/settings.py
+++ src/sciuromorpha_core/settings.py
@@ -10,19 +10,28 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict()
+    model_config = SettingsConfigDict(
+        env_prefix='',
+        extra="ignore",
+        case_sensitive=False,
+        str_strip_whitespace=True,
+    )
 
     mq: AmqpDsn = Field(
         "amqp://guest:guest@localhost:5672/",
         validation_alias=AliasChoices(
-            "rabbit", "rabbmitmq", "rabbitmq_url", "amqp")
+            "mq", "rabbit", "rabbmitmq", "rabbitmq_url", "amqp")
     )
     db: PostgresDsn = Field(
         "postgres://user:pass@localhost:5432/core",
-        validation_alias=AliasChoices("pg", "pg_url"),
+        validation_alias=AliasChoices("db", "db_url", "pg", "pg_url"),
     )
     redis: RedisDsn = Field(
         "redis://user:pass@localhost:6379/1",
-        validation_alias=AliasChoices("redis_url"),
+        validation_alias=AliasChoices("redis", "redis_url"),
     )
     service_name: str = "core"
+    service_mode: str = Field(
+        S.ENV_MODE_DEVELOPMENT,
+        validation_alias=AliasChoices(S.ENV_CONFIG_MODE)
+    )
