ebc22b9d0a5ec5a5c0add25375dfb2e33e93fe8f
Whale Mo <ncwhale@gmail.com>
2023-12-08 17:22:09 +0900

    db session now start with faststream app.

--- src/sciuromorpha_core/db/session.py
+++ src/sciuromorpha_core/db/session.py
@@ -1,18 +1,33 @@
-import sciuromorpha_core.static as S
-from sciuromorpha_core.config import config
-
-if config[S.CONFIG_SECTION_SCIUROMORPHA]["mode"] == S.ENV_MODE_DEVELOPMENT:
-    # We are in development mode, enable logs for SQL.
-    import logging
-
-    logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine(
-    config[S.CONFIG_SECTION_DATABASE]["url"], pool_pre_ping=True, future=True
+from sciuromorpha_core import S, Settings, app
+from faststream import FastStream, Context
+from faststream.rabbit.annotations import (
+    Logger, ContextRepo, RabbitMessage,
+    RabbitBroker as BrokerAnnotation, RabbitProducer, NoCast,
 )
-SessionFactory = sessionmaker(
-    autocommit=False, autoflush=False, bind=engine, future=True
-)
+
+
+@app.on_startup
+async def connect_db(
+        context: ContextRepo,
+        logger: Logger,
+        settings: Settings = Context()):
+    logger.debug("connect_db in db/session.py")
+
+    if settings.core.get("mode", "") == S.ENV_MODE_DEVELOPMENT:
+        # Let sqlalchemy log every SQL in development mode.
+        import logging
+
+        logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
+
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        settings.db, pool_pre_ping=True, future=True
+    )
+    SessionFactory = sessionmaker(
+        autocommit=False, autoflush=False, bind=engine, future=True
+    )
+
+    context.set_global("db_engine", engine)
+    context.set_global("db_session", SessionFactory)
