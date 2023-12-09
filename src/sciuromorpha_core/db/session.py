306364e4537fb7dd9a37ee59cef273c52e201ebf
Whale Mo <ncwhale@gmail.com>
2023-12-09 16:13:56 +0000

    db session add per shutdown hook.

--- src/sciuromorpha_core/db/session.py
+++ src/sciuromorpha_core/db/session.py
@@ -1,16 +1,22 @@
 from sciuromorpha_core import S, Settings, app
 from faststream import FastStream, Context
 from faststream.rabbit.annotations import (
-    Logger, ContextRepo, RabbitMessage,
-    RabbitBroker as BrokerAnnotation, RabbitProducer, NoCast,
+    Logger,
+    ContextRepo,
+    RabbitMessage,
+    RabbitBroker as BrokerAnnotation,
+    RabbitProducer,
+    NoCast,
 )
+
+from sqlalchemy import Engine
+from sqlalchemy.orm import sessionmaker
 
 
 @app.on_startup
 async def connect_db(
-        context: ContextRepo,
-        logger: Logger,
-        settings: Settings = Context()):
+    context: ContextRepo, logger: Logger, settings: Settings = Context()
+):
     logger.debug("connect_db in db/session.py")
 
     if settings.service_mode == S.ENV_MODE_DEVELOPMENT:
@@ -20,14 +26,33 @@ async def connect_db(
         logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 
     from sqlalchemy import create_engine
-    from sqlalchemy.orm import sessionmaker
 
-    engine = create_engine(
-        settings.db, pool_pre_ping=True, future=True
-    )
+    engine = create_engine(settings.db, pool_pre_ping=True, future=True)
     SessionFactory = sessionmaker(
         autocommit=False, autoflush=False, bind=engine, future=True
     )
 
     context.set_global("db_engine", engine)
     context.set_global("db_session", SessionFactory)
+
+
+@app.before_shutdown
+async def disconnect_db(
+    context: ContextRepo,
+    logger: Logger,
+    engine: Engine = Context(),
+    db_session: sessionmaker = Context(),
+):
+    logger.debug("disconnect_db in db/session.py")
+
+    if db_session:
+        context.set_global("db_session", None)
+        db_session.close_all()
+        logger.debug("SessionFactory closed.")
+
+    if engine:
+        context.set_global("db_engine", None)
+        engine.dispose()
+        logger.debug("Engine disposed.")
+
+    logger.debug("disconnect_db done.")
