95745e0844b1dc268740d4d656f9a572ca88428d
Whale Mo <ncwhale@gmail.com>
2023-12-09 17:28:54 +0000

    Migreate secret rpc.

--- src/sciuromorpha_core/rpc/task.py
+++ src/sciuromorpha_core/rpc/task.py
@@ -12,6 +12,7 @@ from faststream.rabbit.annotations import (
 from sqlalchemy.orm import joinedload, sessionmaker
 
 from sciuromorpha_core import model
+from sciuromorpha_core.db import session
 from sciuromorpha_core.app import app, broker
 from .mq_schema import task_rpc, task_topic
 
