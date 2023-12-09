67645c6c16945fdf443510c9549540c45da7dff2
Whale Mo <ncwhale@gmail.com>
2023-12-09 17:05:14 +0000

    Remove debug codes.

--- src/sciuromorpha_core/app.py
+++ src/sciuromorpha_core/app.py
@@ -1,15 +1,21 @@
 import os
+from uuid import uuid4
 from sciuromorpha_core import static as S
 from sciuromorpha_core import Settings
 from faststream import FastStream, Context
 from faststream.rabbit import RabbitBroker
 from faststream.rabbit.annotations import (
-    Logger, ContextRepo, RabbitMessage,
-    RabbitBroker as BrokerAnnotation, RabbitProducer, NoCast,
+    Logger,
+    ContextRepo,
+    RabbitMessage,
+    RabbitBroker as BrokerAnnotation,
+    RabbitProducer,
+    NoCast,
 )
 
 broker = RabbitBroker()
 app = FastStream(broker)
+app_id = str(uuid4())
 
 
 @app.on_startup
@@ -19,18 +25,10 @@ async def setup(context: ContextRepo, env: str = ".env"):
 
 
 @app.on_startup
-async def connect_db(
-        logger: Logger,
-        settings: Settings = Context()):
-    # TODO: migration db struct here.
-    logger.debug("connect_db in app.py")
-    pass
-
-
-@app.on_startup
-async def connect_mq(settings: Settings = Context()):
+async def connect_mq(logger: Logger, settings: Settings = Context()):
     # Everything is OK, let's connect to mq and start service.
     await broker.connect(str(settings.mq))
+    logger.debug("connect to mq success")
 
 
 @app.after_startup
@@ -39,7 +37,27 @@ async def publish_online(
     broker: BrokerAnnotation,
 ):
     # Publish online message to broker.
-    await broker.publish({
-        "status": "ONLINE",
-    }, queue="service-core")
+    await broker.publish(
+        {
+            "status": "ONLINE",
+            "app_id": app_id,
+        },
+        queue="service-core",
+    )
+    logger.info("Core service startup success.")
+
+
+@app.on_shutdown
+async def publish_offline(
+    logger: Logger,
+    broker: BrokerAnnotation,
+):
+    # Publish online message to broker.
+    await broker.publish(
+        {
+            "status": "OFFLINE",
+            "app_id": app_id,
+        },
+        queue="service-core",
+    )
     logger.info("Core service startup success.")
